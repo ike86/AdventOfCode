7167e5a6247fd0b8b6046e3d0b5e8cfaf63824ba
Pfeff Ákos György <csopimaci@gmail.com>
2017-12-19 21:03:51 +0100

    add solution of Day01 part1
    result of pairing with @robotjatek

---
+++ src/AdventOfCode.InverseCaptcha/CaptchaInverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode.InverseCaptcha
+{
+    internal class CaptchaInverter
+    {
+        private string input;
+
+        public CaptchaInverter(string input)
+        {
+            this.input = input;
+        }
+
+        internal int GetSum()
+        {
+            if (!input.Any())
+            {
+                return 0;
+            }
+
+            var sum = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == input[(i + 1) % input.Length])
+                {
+                    sum += int.Parse(input[i].ToString());
+                }
+            }
+
+            return sum;
+        }
+    }
+}
