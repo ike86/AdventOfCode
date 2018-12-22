c6b68460083bee72bed7e7a2a6e76b8d0e966e6d
Pfeff Ákos György <ike86@users.noreply.github.com>
2018-12-22 12:35:15 +0100

    day 1 pr1 Returns_zero_for_no_input

---
+++ src/AdventOfCode2018/Day01/ChronalCalibratior_Calibrate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode2018.Day01
+{
+    public class ChronalCalibratior_Calibrate
+    {
+        [Fact]
+        public void Returns_zero_for_no_input()
+        {
+            var calibrator = new ChronalCalibrator();
+
+            var result = calibrator.Calibrate(string.Empty);
+
+            result.Should().Be(0);
+        }
+    }
+}
---
+++ src/AdventOfCode2018/Day01/ChronalCalibrator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AdventOfCode2018.Day01
+{
+    internal class ChronalCalibrator
+    {
+        public ChronalCalibrator()
+        {
+        }
+
+        internal int Calibrate(string empty)
+        {
+            return 0;
+        }
+    }
+}
