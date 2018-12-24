ee3008e1fed545d8096006a96880c81da53f0b1d
Pfeff Ákos György <ike86@users.noreply.github.com>
2018-12-24 11:47:08 +0100

    day 2 pr1 green first test

---
+++ src/AdventOfCode2018/Day02/InventoryManagementSystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AoC18.Day02
+{
+    public class InventoryManagementSystem
+    {
+        public InventoryManagementSystem()
+        {
+        }
+
+        public int GetCheckSum(string v)
+        {
+            return 0;
+        }
+    }
+}
---
+++ src/AdventOfCode2018/Day02/InventoryManagementSystem_GetCheckSum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace AoC18.Day02
+{
+    public class InventoryManagementSystem_GetCheckSum
+    {
+        [Fact]
+        public void asd()
+        {
+            var ims = new InventoryManagementSystem();
+
+            int checkSum = ims.GetCheckSum("abcdef");
+
+            checkSum.Should().Be(0);
+        }
+    }
+}
