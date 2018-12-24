59a2ef4a128f5b13eb9d8d68a222495ca1f71d07
Pfeff Ákos György <ike86@users.noreply.github.com>
2018-12-24 23:28:59 +0100

    day 2 pr1 fix tests

--- src/AdventOfCode2018/Day02/InventoryManagementSystem_GetCheckSum.cs
+++ src/AdventOfCode2018/Day02/InventoryManagementSystem_GetCheckSum.cs
@@ -19,7 +19,18 @@ namespace AoC18.Day02
         [InlineData("AABBcde")]
         [InlineData("AAAbcd")]
         [InlineData("AAABBBcde")]
-        public void Returns_one_for_single_input_with_some_characters_appearing_twice_xor_three_times(string input)
+        public void Returns_zero_for_single_input_with_some_characters_appearing_twice_xor_three_times(string input)
+        {
+            var checkSum = InventoryManagementSystem.GetCheckSum(input);
+
+            checkSum.Should().Be(0);
+        }
+
+        [Theory]
+        [InlineData("AAABBcde")]
+        [InlineData("AABBBcd")]
+        [InlineData("AABBcdEEEf")]
+        public void Returns_one_for_single_input_with_some_characters_appearing_twice_and_three_times(string input)
         {
             var checkSum = InventoryManagementSystem.GetCheckSum(input);
 
@@ -29,7 +40,10 @@ namespace AoC18.Day02
         [Fact]
         public void Returns_one_for_many_input()
         {
-            var input = "abcdef" + InventoryManagementSystem.LineSeparator + "AAbcdef";
+            var input =
+                "abcdef" + InventoryManagementSystem.LineSeparator +
+                "AAbcd" + InventoryManagementSystem.LineSeparator +
+                "AAAbcd";
 
             var checkSum = InventoryManagementSystem.GetCheckSum(input);
 
