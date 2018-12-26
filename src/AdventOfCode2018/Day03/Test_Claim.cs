733a88d5fb1e7a3db1c964014954dc04735c4b3d
Pfeff Ákos György <ike86@users.noreply.github.com>
2018-12-26 20:28:37 +0100

    d3p1 refactor tests

--- src/AdventOfCode2018/Day03/Test_Claim.cs
+++ src/AdventOfCode2018/Day03/Test_Claim.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using AutoFixture.Xunit2;
+using FluentAssertions;
 using FluentAssertions.Execution;
 using Xunit;
 
@@ -6,27 +7,27 @@ namespace AoC18.Day03
 {
     public class Test_Claim
     {
-        [Fact]
-        public void Parse_respects_x_offset()
+        [Theory, AutoData]
+        public void Parse_respects_x_offset(int xOffset)
         {
-            var claim = Claim.Parse("#1 @ 1,3: 4x4");
+            var claim = Claim.Parse($"#1 @ {xOffset},3: 4x4");
 
             using (new AssertionScope())
             {
                 claim[0, 3].Should().Be(0);
-                claim[1, 3].Should().Be(1);
+                claim[xOffset, 3].Should().Be(1);
             }
         }
 
-        [Fact]
-        public void Parse_respects_y_offset()
+        [Theory, AutoData]
+        public void Parse_respects_y_offset(int yOffset)
         {
-            var claim = Claim.Parse("#1 @ 1,3: 4x4");
+            var claim = Claim.Parse($"#1 @ 1,{yOffset}: 4x4");
 
             using (new AssertionScope())
             {
                 claim[1, 0].Should().Be(0);
-                claim[1, 3].Should().Be(1);
+                claim[1, yOffset].Should().Be(1);
             }
         }
     }
