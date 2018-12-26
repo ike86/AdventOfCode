d52c0091d7d4689d1b0060429ed894a9bdd1d8a3
Pfeff Ákos György <ike86@users.noreply.github.com>
2018-12-26 18:23:17 +0100

    d2p2 green Returns_empty_for_two_different_characters

---
+++ src/AdventOfCode2018/Day02/Part2.cs
@@ -0,0 +1,10 @@
+﻿namespace AoC18.Day02
+{
+    internal class Part2
+    {
+        internal static string GetCommonLetters(string first, string second)
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ src/AdventOfCode2018/Day02/Test_GetCommonLetters.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Xunit;
+using static AoC18.Day02.Part2;
+
+namespace AoC18.Day02
+{
+    public class GetCommonLetters
+    {
+        [Fact]
+        public void Returns_empty_for_two_different_characters()
+        {
+            var result = GetCommonLetters("a", "b");
+
+            result.Should().Be(string.Empty);
+        }
+    }
+}
