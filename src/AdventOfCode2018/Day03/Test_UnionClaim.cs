e1df414d9ae0befc2687232e38e41d602de5536c
Pfeff Ákos György <ike86@users.noreply.github.com>
2018-12-26 21:23:20 +0100

    d3p1 green Test_UnionClaim Is_idempotent

---
+++ src/AdventOfCode2018/Day03/Test_UnionClaim.cs
@@ -0,0 +1,17 @@
+﻿using AutoFixture.Xunit2;
+using FluentAssertions;
+using Xunit;
+
+namespace AoC18.Day03
+{
+    public class Test_UnionClaim
+    {
+        [Theory, AutoData]
+        public void Is_idempotent(Claim claim)
+        {
+            var union = new UnionClaim(claim);
+
+            union.Should().BeEquivalentTo(claim);
+        }
+    }
+}
---
+++ src/AdventOfCode2018/Day03/UnionClaim.cs
@@ -0,0 +1,20 @@
+﻿namespace AoC18.Day03
+{
+    internal class UnionClaim
+    {
+        private Claim claim;
+
+        public UnionClaim(Claim claim)
+        {
+            this.claim = claim;
+        }
+
+        public int XOffset => claim.XOffset;
+
+        public int YOffset => claim.YOffset;
+
+        public int Width => claim.Width;
+
+        public int Height => claim.Height;
+    }
+}
