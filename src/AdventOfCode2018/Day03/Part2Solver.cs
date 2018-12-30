59f6230d3a623bb84b7b900fec2e562b00ac6f6f
Pfeff Ákos György <ike86@users.noreply.github.com>
2018-12-30 23:58:30 +0100

    d3p2 green Solution2_returns_id_of_single_claim

---
+++ src/AdventOfCode2018/Day03/Part2Solver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AoC18.Day03
+{
+    internal class Part2Solver
+    {
+        private Claim claim;
+
+        public Part2Solver(Claim claim)
+        {
+            this.claim = claim;
+        }
+
+        internal int GetIdOfOnlyNonOverlappingClaim()
+        {
+            return claim.Id;
+        }
+    }
+}
--- src/AdventOfCode2018/Day03/Solution.cs
+++ src/AdventOfCode2018/Day03/Solution.cs
@@ -1,5 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
+using AutoFixture.Xunit2;
 using FluentAssertions;
 using Xunit;
 
@@ -36,6 +38,46 @@ namespace AoC18.Day03
             var result = Solve(input);
 
             result.Should().Be(105231);
+        }
+
+        [Theory, AutoData]
+        public void Solution2_returns_id_of_single_claim(Claim claim)
+        {
+            var part2solver = new Part2Solver(claim);
+
+            var id = part2solver.GetIdOfOnlyNonOverlappingClaim();
+
+            id.Should().Be(claim.Id);
+        }
+
+        public static int SolvePart2(string[] input)
+        {
+            var claims = input
+                .Select(i => Claim.Parse(i))
+                .ToArray();
+            var union = new UnionClaim(claims);
+
+            foreach (var claim in claims)
+            {
+                var isMatching = true;
+                for (int i = claim.XOffset; i < claim.BottomRightX; i++)
+                {
+                    for (int j = claim.YOffset; j < claim.BottomRightY; j++)
+                    {
+                        if (isMatching && union[i, j] >= 2)
+                        {
+                            isMatching = false;
+                        }
+                    }
+                }
+
+                if (isMatching)
+                {
+                    return claim.Id;
+                }
+            }
+
+            throw new ArgumentException();
         }
     }
 }
