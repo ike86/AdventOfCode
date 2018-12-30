07aeec83292c5b2b642fc79e0a46a33bdfd0bb44
Pfeff Ákos György <ike86@users.noreply.github.com>
2018-12-30 19:55:06 +0100

    d3p1 green Has_maximum_bottom_right_x

--- src/AdventOfCode2018/Day03/Test_UnionClaim.cs
+++ src/AdventOfCode2018/Day03/Test_UnionClaim.cs
@@ -53,6 +53,15 @@ namespace AoC18.Day03
                     claims.Select(c => c.Height).Max());
         }
 
+        [Theory, AutoData]
+        public void Has_maximum_bottom_right_x(Claim[] claims)
+        {
+            new UnionClaim(claims)
+                .BottomRightX
+                .Should().Be(
+                    claims.Select(c => c.BottomRightX).Max());
+        }
+
         /*     0    1    2    3  4  5
          *     C1   A1   B1   C2 A2 B2
          *          A------------*     A1 0
