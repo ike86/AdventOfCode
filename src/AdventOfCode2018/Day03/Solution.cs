1e93539216f2f4c3becb306a6622318bb08b10ee
Pfeff Ákos György <ike86@users.noreply.github.com>
2018-12-30 20:37:58 +0100

    solve day 3 part 1

---
+++ src/AdventOfCode2018/Day03/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace AoC18.Day03
+{
+    public class Solution
+    {
+        public static int Solve(string[] input)
+        {
+            var union = new UnionClaim(
+                input
+                .Select(i => Claim.Parse(i))
+                .ToArray());
+            var numberOfSquareInchesWithinTwoOrMoreClaims = 0;
+            for (int i = 0; i < union.XOffset + union.Width; i++)
+            {
+                for (int j = 0; j < union.YOffset + union.Height; j++)
+                {
+                    if (union[i, j] >= 2)
+                    {
+                        numberOfSquareInchesWithinTwoOrMoreClaims += 1;
+                    }
+                }
+            }
+
+            return numberOfSquareInchesWithinTwoOrMoreClaims;
+        }
+
+        [Fact]
+        public void _of_day_3_part_1()
+        {
+            var input = File.ReadAllLines("Day03/input.txt");
+
+            var result = Solve(input);
+
+            result.Should().Be(105231);
+        }
+    }
+}
