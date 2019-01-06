9b907013f992b40dae57214e7f60bb44c55ff425
Pfeff Ákos György <ike86@users.noreply.github.com>
2019-01-06 17:38:57 +0100

    d4p1 green first test

---
+++ src/AdventOfCode2018/Day04/Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AoC18.Day04
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test01()
+        {
+            var records = ReposeRecord("[1518-11-01 00:00] Guard #10 begins shift");
+        }
+
+        private object ReposeRecord(string v)
+        {
+            return null;
+        }
+    }
+}
