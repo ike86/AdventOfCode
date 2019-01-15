e1f87db55d30e8ea732f287016203cd86f724eab
Pfeff Ákos György <ike86@users.noreply.github.com>
2019-01-15 20:22:24 +0100

    d4p1 green 4 first test

--- src/AdventOfCode2018/Day04/Tests.cs
+++ src/AdventOfCode2018/Day04/Tests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
 using Xunit;
 
 namespace AoC18.Day04
@@ -15,18 +16,33 @@ namespace AoC18.Day04
             var records = ReposeRecord("[1518-11-01 00:00] Guard #10 begins shift");
 
             var id = records.Of(11, 01).GuardId;
+
+            id.Should().Be(10);
         }
 
         private GuardRecords ReposeRecord(string v)
         {
-            return new GuardRecords();
+            return new GuardRecords(v);
         }
 
         private class GuardRecords
         {
+            private GuardRecord guardRecord;
+
+            public GuardRecords(string v)
+            {
+                this.guardRecord =
+                    new GuardRecord
+                    {
+                        GuardId = int.Parse(
+                            v.Split(new[] { ' ', '#' }, StringSplitOptions.RemoveEmptyEntries)
+                            [3])
+                    };
+            }
+
             internal GuardRecord Of(int v1, int v2)
             {
-                return new GuardRecord();
+                return this.guardRecord;
             }
 
             internal class GuardRecord
