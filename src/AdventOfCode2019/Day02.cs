d9c71cb723326b3e439d2d72b12941d282372e57
Pfeff Ákos György <ike86@users.noreply.github.com>
2019-12-20 13:54:32 +0100

    day 2 part 2 push instruction pointer offset to operations
    and by that, remove hard-coded offset
    - replace tuple with ExecutionResult

--- src/AdventOfCode2019/Day02.cs
+++ src/AdventOfCode2019/Day02.cs
@@ -143,19 +143,18 @@ namespace AoC19
                 {
                     var intruction = new Instruction(Code, instructionPointer);
 
-                    int result;
-                    int instructionPointerOffset;
+                    ExecutionResult result;
                     try
                     {
-                        (result, instructionPointerOffset) = intruction.Execute();
-                        Code[intruction.AddressOfResult] = result;
+                        result = intruction.Execute();
+                        Code[intruction.AddressOfResult] = result.Value;
                     }
                     catch (ProgramHalted)
                     {
                         break;
                     }
 
-                    instructionPointer += instructionPointerOffset;
+                    instructionPointer += result.InstructionPointerOffset;
                 }
             }
 
@@ -163,13 +162,13 @@ namespace AoC19
             {
                 private readonly int[] code;
                 private readonly int instructionPointer;
-                private readonly Func<Func<int>, Func<int>, int> execute;
+                private readonly IOperation operation;
 
                 public Instruction(IEnumerable<int> code, int instructionPointer)
                 {
                     this.code = EnsureHasTrailingPositions(code);
                     this.instructionPointer = instructionPointer;
-                    execute = Create(OpCode).Execute;
+                    operation = Create(OpCode);
                 }
 
                 public int AddressOfResult => code[instructionPointer + 3];
@@ -184,9 +183,12 @@ namespace AoC19
 
                 private int OpCode => code[instructionPointer + 0];
 
-                public (int result, int instructionPointerOffset) Execute()
+                public ExecutionResult Execute()
                 {
-                    return (execute(LeftOperand, RightOperand), 4);
+                    return 
+                        new ExecutionResult(
+                            operation.Execute(LeftOperand, RightOperand),
+                            operation.InstructionPointerOffset);
                 }
 
                 private int[] EnsureHasTrailingPositions(IEnumerable<int> code)
@@ -211,23 +213,48 @@ namespace AoC19
 
                 private interface IOperation
                 {
+                    int InstructionPointerOffset { get; }
+
                     int Execute(Func<int> x, Func<int> y);
                 }
 
                 private class Addition : IOperation
                 {
+                    public int InstructionPointerOffset => 4;
+
+                    // the number of operation arguments is still hard-coded,
+                    // which forces us to use lazy argument resolution in order to not fail on Halt
                     public int Execute(Func<int> x, Func<int> y) => x() + y();
                 }
 
                 private class Multiplication : IOperation
                 {
+                    public int InstructionPointerOffset => 4;
+
                     public int Execute(Func<int> x, Func<int> y) => x() * y();
                 }
 
                 private class Halting : IOperation
                 {
+                    public int InstructionPointerOffset => 0;
+
                     public int Execute(Func<int> x, Func<int> y) => throw new ProgramHalted();
                 }
+            }
+
+            private class ExecutionResult
+            {
+                public ExecutionResult(
+                    int value,
+                    int instructionPointerOffset)
+                {
+                    Value = value;
+                    InstructionPointerOffset = instructionPointerOffset;
+                }
+
+                public int Value { get; }
+
+                public int InstructionPointerOffset { get; }
             }
 
             private class ProgramHalted : Exception { }
