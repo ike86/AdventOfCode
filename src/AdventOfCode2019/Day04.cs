bcb8b15d397f199f38e0034742920ea99e484564
Pfeff Ákos György <ike86@users.noreply.github.com>
2020-01-01 13:21:26 +0100

    day 4 part 1 refactor

--- src/AdventOfCode2019/Day04.cs
+++ src/AdventOfCode2019/Day04.cs
@@ -10,25 +10,28 @@ namespace AoC19
 {
     public class Day04
     {
+        private const int Start = 100000;
+        private const int End = 999999;
+
         /*
-        --- Day 4: Secure Container ---
-        You arrive at the Venus fuel depot only to discover it's protected by a password.
-        The Elves had written the password on a sticky note, but someone threw it out.
+--- Day 4: Secure Container ---
+You arrive at the Venus fuel depot only to discover it's protected by a password.
+The Elves had written the password on a sticky note, but someone threw it out.
 
-        However, they do remember a few key facts about the password:
+However, they do remember a few key facts about the password:
 
-        It is a six-digit number.
-        The value is within the range 172930-683082.
-        Two adjacent digits are the same (like 22 in 122345).
-        Going from left to right, the digits never decrease;
-        they only ever increase or stay the same (like 111123 or 135679).
-        Other than the range rule, the following are true:
+It is a six-digit number.
+The value is within the range 172930-683082.
+Two adjacent digits are the same (like 22 in 122345).
+Going from left to right, the digits never decrease;
+they only ever increase or stay the same (like 111123 or 135679).
+Other than the range rule, the following are true:
 
-        111111 meets these criteria (double 11, never decreases).
-        223450 does not meet these criteria (decreasing pair of digits 50).
-        123789 does not meet these criteria (no double).
-        How many different passwords within the range given in your puzzle input meet these criteria?
-         */
+111111 meets these criteria (double 11, never decreases).
+223450 does not meet these criteria (decreasing pair of digits 50).
+123789 does not meet these criteria (no double).
+How many different passwords within the range given in your puzzle input meet these criteria?
+*/
 
         [Fact]
         void Is_a_six_digit_number()
@@ -36,15 +39,15 @@ namespace AoC19
             IEnumerable<int> passwords = PossiblePasswords();
 
             using var a = new AssertionScope();
-            passwords.Should().NotContain(99999);
-            passwords.Should().Contain(100000);
-            passwords.Should().Contain(999999);
-            passwords.Should().NotContain(1000000);
+            passwords.Should().NotContain(Start - 1);
+            passwords.Should().Contain(Start);
+            passwords.Should().Contain(End);
+            passwords.Should().NotContain(End + 1);
         }
 
         private IEnumerable<int> PossiblePasswords()
         {
-            return Enumerable.Range(100000, 999999 - 100000 +1);
+            return Enumerable.Range(Start, End - Start + 1);
         }
     }
 }
