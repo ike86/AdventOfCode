1e870e0e2a69cdc2c3f54566af538f5e28a67661
Pfeff Ákos György <ike86@users.noreply.github.com>
2020-01-01 13:17:04 +0100

    day 4 part 1 pass Is_a_six_digit_number

--- src/AdventOfCode2019/Day04.cs
+++ src/AdventOfCode2019/Day04.cs
@@ -18,7 +18,7 @@ namespace AoC19
         However, they do remember a few key facts about the password:
 
         It is a six-digit number.
-        The value is within the range given in your puzzle input.
+        The value is within the range 172930-683082.
         Two adjacent digits are the same (like 22 in 122345).
         Going from left to right, the digits never decrease;
         they only ever increase or stay the same (like 111123 or 135679).
@@ -28,8 +28,19 @@ namespace AoC19
         223450 does not meet these criteria (decreasing pair of digits 50).
         123789 does not meet these criteria (no double).
         How many different passwords within the range given in your puzzle input meet these criteria?
-
-        Your puzzle input is 172930-683082.
          */
+
+        [Fact]
+        void Is_a_six_digit_number()
+        {
+            IEnumerable<int> passwords = PossiblePasswords();
+
+            passwords.Should().Contain(100000);
+        }
+
+        private IEnumerable<int> PossiblePasswords()
+        {
+            yield return 100000;
+        }
     }
 }
