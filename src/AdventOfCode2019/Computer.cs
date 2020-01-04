113716c87840a6c9b513322f23bbcb6b765b5eeb
Pfeff Ákos György <ike86@users.noreply.github.com>
2020-01-04 11:55:09 +0100

    day 5 part 1 publish classes Computer and Program
    so that they can be reused

---
+++ src/AdventOfCode2019/Computer.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+namespace AoC19
+{
+    class Computer
+    {
+        public Computer(string programCode)
+        {
+            Memory =
+                programCode
+                .Split(',')
+                .Select(int.Parse)
+                .ToArray();
+        }
+
+        public int[] Memory { get; private set; }
+
+        public int Run()
+        {
+            var program = new Program(Memory);
+            program.Run();
+            return program.Code[0];
+        }
+    }
+}
--- src/AdventOfCode2019/Day05.cs
+++ src/AdventOfCode2019/Day05.cs
@@ -13,9 +13,6 @@ namespace AoC19
     {
         /*
         --- Day 5: Sunny with a Chance of Asteroids ---
-        You're starting to sweat as the ship makes its way toward Mercury.
-        The Elves suggest that you get the air conditioner working
-        by upgrading your ship computer to support the Thermal Environment Supervision Terminal.
 
         The Thermal Environment Supervision Terminal (TEST)
         starts by running a diagnostic program (your puzzle input).
