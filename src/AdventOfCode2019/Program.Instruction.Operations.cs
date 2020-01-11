40b9066e8e94d14334287ddce2385685d6a7d5cc
Pfeff Ákos György <ike86@users.noreply.github.com>
2020-01-11 16:58:54 +0100

    day 5 part 1 decrease Program.cs file size
    by moving out Instruction operators

---
+++ src/AdventOfCode2019/Program.Instruction.Operations.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC19
+{
+    partial class Program
+    {
+        private partial class Instruction
+        {
+            private interface IOperation
+            {
+                int NumberOfParameters { get; }
+
+                int InstructionPointerOffset { get; }
+            }
+
+            private interface IFunction : IOperation
+            {
+                int Execute(IEnumerable<Func<int>> arguments);
+            }
+
+            private interface IAction : IOperation
+            {
+                void Execute(IEnumerable<Func<int>> arguments);
+            }
+
+            private class Addition : IFunction
+            {
+                public int InstructionPointerOffset => 4;
+
+                public int NumberOfParameters => 2;
+
+                // the number of operation arguments is still hard-coded,
+                // which forces us to use lazy argument resolution in order to not fail on Halt
+                public int Execute(IEnumerable<Func<int>> arguments)
+                {
+                    var args = arguments.ToArray();
+                    return args[0]() + args[1]();
+                }
+            }
+
+            private class Multiplication : IFunction
+            {
+                public int InstructionPointerOffset => 4;
+
+                public int NumberOfParameters => 2;
+
+                public int Execute(IEnumerable<Func<int>> arguments)
+                {
+                    var args = arguments.ToArray();
+                    return args[0]() * args[1]();
+                }
+            }
+
+            private class Halting : IAction
+            {
+                public int InstructionPointerOffset => 0;
+
+                public int NumberOfParameters => 0;
+
+                public void Execute(IEnumerable<Func<int>> arguments) => throw new ProgramHalted();
+            }
+
+            private class Input : IFunction
+            {
+                private readonly Func<int> readInput;
+
+                public Input(Func<int> readInput)
+                {
+                    this.readInput = readInput;
+                }
+
+                public int InstructionPointerOffset => 2;
+
+                public int NumberOfParameters => 0;
+
+                public int Execute(IEnumerable<Func<int>> arguments) => readInput();
+            }
+
+            private class Output : IAction
+            {
+                private readonly Action<int> writeOutput;
+
+                public Output(Action<int> writeOutput)
+                {
+                    this.writeOutput = writeOutput;
+                }
+
+                public int InstructionPointerOffset => 2;
+
+                public int NumberOfParameters => 1;
+
+                public void Execute(IEnumerable<Func<int>> arguments)
+                {
+                    var args = arguments.ToArray();
+                    writeOutput(args[0]());
+                }
+            }
+        }
+    }
+}
--- src/AdventOfCode2019/Program.cs
+++ src/AdventOfCode2019/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace AoC19
 {
-    class Program
+    partial class Program
     {
         private readonly Func<int> readInput;
         private readonly Action<int> writeOutput;
@@ -48,7 +48,7 @@ namespace AoC19
             }
         }
 
-        private class Instruction
+        private partial class Instruction
         {
             private readonly int[] code;
             private readonly int instructionPointer;
@@ -115,96 +115,6 @@ namespace AoC19
             private Func<int> NthOperand(int n) => () => code[AddressOfNthOperand(n)];
 
             private int AddressOfNthOperand(int n) => code[instructionPointer + n];
-
-            private interface IOperation
-            {
-                int NumberOfParameters { get; }
-
-                int InstructionPointerOffset { get; }
-            }
-
-            private interface IFunction : IOperation
-            {
-                int Execute(IEnumerable<Func<int>> arguments);
-            }
-
-            private interface IAction : IOperation
-            {
-                void Execute(IEnumerable<Func<int>> arguments);
-            }
-
-            private class Addition : IFunction
-            {
-                public int InstructionPointerOffset => 4;
-
-                public int NumberOfParameters => 2;
-
-                // the number of operation arguments is still hard-coded,
-                // which forces us to use lazy argument resolution in order to not fail on Halt
-                public int Execute(IEnumerable<Func<int>> arguments)
-                {
-                    var args = arguments.ToArray();
-                    return args[0]() + args[1]();
-                }
-            }
-
-            private class Multiplication : IFunction
-            {
-                public int InstructionPointerOffset => 4;
-
-                public int NumberOfParameters => 2;
-
-                public int Execute(IEnumerable<Func<int>> arguments)
-                {
-                    var args = arguments.ToArray();
-                    return args[0]() * args[1]();
-                }
-            }
-
-            private class Halting : IAction
-            {
-                public int InstructionPointerOffset => 0;
-
-                public int NumberOfParameters => 0;
-
-                public void Execute(IEnumerable<Func<int>> arguments) => throw new ProgramHalted();
-            }
-
-            private class Input : IFunction
-            {
-                private readonly Func<int> readInput;
-
-                public Input(Func<int> readInput)
-                {
-                    this.readInput = readInput;
-                }
-
-                public int InstructionPointerOffset => 2;
-
-                public int NumberOfParameters => 0;
-
-                public int Execute(IEnumerable<Func<int>> arguments) => readInput();
-            }
-
-            private class Output : IAction
-            {
-                private readonly Action<int> writeOutput;
-
-                public Output(Action<int> writeOutput)
-                {
-                    this.writeOutput = writeOutput;
-                }
-
-                public int InstructionPointerOffset => 2;
-
-                public int NumberOfParameters => 1;
-
-                public void Execute(IEnumerable<Func<int>> arguments)
-                {
-                    var args = arguments.ToArray();
-                    writeOutput(args[0]());
-                }
-            }
         }
 
         private class ExecutionResult
