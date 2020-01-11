255cb19a8fccb169793470f6e0a63d0c60d97c28
Pfeff Ákos György <ike86@users.noreply.github.com>
2020-01-11 20:12:41 +0100

    day 5 part 1 reduce size of Program.cs
    by moving out ExecutionResults

---
+++ src/AdventOfCode2019/Program.ExecutionResult.cs
@@ -0,0 +1,35 @@
+﻿namespace AoC19
+{
+    partial class Program
+    {
+        private interface IExecutionResult
+        {
+            int InstructionPointerOffset { get; }
+        }
+
+        private class FunctionExecutionResult : IExecutionResult
+        {
+            public FunctionExecutionResult(
+                int value,
+                int instructionPointerOffset)
+            {
+                Value = value;
+                InstructionPointerOffset = instructionPointerOffset;
+            }
+
+            public int Value { get; }
+
+            public int InstructionPointerOffset { get; }
+        }
+
+        private class ActionExecutionResult : IExecutionResult
+        {
+            public ActionExecutionResult(int instructionPointerOffset)
+            {
+                InstructionPointerOffset = instructionPointerOffset;
+            }
+
+            public int InstructionPointerOffset { get; }
+        }
+    }
+}
--- src/AdventOfCode2019/Program.cs
+++ src/AdventOfCode2019/Program.cs
@@ -114,36 +114,6 @@ namespace AoC19
             private int AddressOfNthOperand(int n) => code[instructionPointer + n];
         }
 
-        private interface IExecutionResult
-        {
-            int InstructionPointerOffset { get; }
-        }
-
-        private class FunctionExecutionResult : IExecutionResult
-        {
-            public FunctionExecutionResult(
-                int value,
-                int instructionPointerOffset)
-            {
-                Value = value;
-                InstructionPointerOffset = instructionPointerOffset;
-            }
-
-            public int Value { get; }
-
-            public int InstructionPointerOffset { get; }
-        }
-
-        private class ActionExecutionResult : IExecutionResult
-        {
-            public ActionExecutionResult(int instructionPointerOffset)
-            {
-                InstructionPointerOffset = instructionPointerOffset;
-            }
-
-            public int InstructionPointerOffset { get; }
-        }
-
         private class ProgramHalted : Exception { }
     }
 }
