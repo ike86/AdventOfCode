0df575e1634926de5f4e841a680d9ac44e8f5b8a
Pfeff Ákos György <ike86@users.noreply.github.com>
2020-11-15 19:40:30 +0100

    separate unrelated test modules

--- src/AoC19.Fs/AoC19/Tests.fs
+++ src/AoC19.Fs/AoC19/Tests.fs
@@ -1,22 +1,24 @@
 ﻿namespace AoC19.Tests
 
-    open Xunit
-    open FsUnit.Xunit
-    open AoC19
-    open Intcode
+open Xunit
+open FsUnit.Xunit
+open AoC19
+open Intcode
 
-    module ``Intcode tests`` =
-        module ``Given an Intcode program code`` =
-            [<Fact>]
-            let ``parse yields the Intcode.Program`` ()=
-                parse "1,0,0,3,99" |> should equal [|1;0;0;3;99|]
+module ``Given an Intcode program code`` =
+    [<Fact>]
+    let ``parse yields the Intcode.Program`` () =
+        parse "1,0,0,3,99"
+        |> should equal [| 1; 0; 0; 3; 99 |]
 
-        [<Fact>]
-        let ``run halts on 99`` ()=
-            (fun () -> (run (parse "99")) |> ignore)
-            |> should throw typeof<ProgramHaltedException>
+module ``Intcode tests`` =
 
-        [<Fact>]
-        let ``Opcode 1 adds together numbers`` ()=
-            let output: Program = run (parse "1,5,6,7,99,4,5,0")
-            Array.last output |> should equal (4 + 5)
+    [<Fact>]
+    let ``run halts on 99`` () =
+        (fun () -> (run (parse "99")) |> ignore)
+        |> should throw typeof<ProgramHaltedException>
+
+    [<Fact>]
+    let ``Opcode 1 adds together numbers`` () =
+        let output: Program = run (parse "1,5,6,7,99,4,5,0")
+        Array.last output |> should equal (4 + 5)
