d999aa2cab6bf87025a3fec09b95bf46163dc3c1
Pfeff Ákos György <ike86@users.noreply.github.com>
2020-11-15 21:20:20 +0100

    refactor run to (partially) use terminology

--- src/AoC19.Fs/AoC19/Library.fs
+++ src/AoC19.Fs/AoC19/Library.fs
@@ -101,7 +101,7 @@ module Intcode =
             let outputIndex = program.[i + 3]
             let output = left + right
             Array.set program outputIndex output
-            program
+            4
 
         let multiply (program: Program) i =
             let leftIndex = program.[i + 1]
@@ -111,54 +111,31 @@ module Intcode =
             let outputIndex = program.[i + 3]
             let output = left * right
             Array.set program outputIndex output
-            program
+            4
 
-        let mutable p = program
         try
-            let mutable i = 0
+            let mutable instructionPointer = 0
             while true do
-                p <-
-                    match program.[i] with
+                let offset =
+                    match program.[instructionPointer] with
+                    | 1 -> add program instructionPointer
+                    | 2 -> multiply program instructionPointer
                     | 99 -> raise ProgramHaltedException
-                    | 1 -> add program i
-                    | 2 -> multiply program i
                     | _ -> failwith "unexpected opcode"
-                i <- i + 4
-            p
-        with ProgramHaltedException -> p
+                instructionPointer <- instructionPointer + offset
+            program
+        with ProgramHaltedException -> program
 
-// "Good, the new computer seems to be working correctly!
-// Keep it nearby during this mission - you'll probably use it again.
-// Real Intcode computers support many more features than your new one,
-// but we'll let you know what they are as you need them."
+// Terminology not yet addressed in the code:
 //
-// "However, your current priority should be to complete
-// your gravity assist around the Moon. For this mission to succeed,
-// we should settle on some terminology for the parts you've already built."
-//
-// Intcode programs are given as a list of integers;
-// these values are used as the initial state for the computer's memory.
-// When you run an Intcode program, make sure to start by initializing memory
-// to the program's values. A position in memory is called an address
-// (for example, the first value in memory is at "address 0").
+// A position in memory is called an address.
 //
 // Opcodes (like 1, 2, or 99) mark the beginning of an instruction.
 // The values used immediately after an opcode, if any,
 // are called the instruction's parameters.
-// For example, in the instruction 1,2,3,4, 1 is the opcode;
-// 2, 3, and 4 are the parameters.
 // The instruction 99 contains only an opcode and has no parameters.
-//
-// The address of the current instruction is called the instruction pointer;
-// it starts at 0. After an instruction finishes,
-// the instruction pointer increases by the number of values in the instruction;
-// until you add more instructions to the computer,
-// this is always 4 (1 opcode + 3 parameters) for the add and multiply instructions.
-// (The halt instruction would increase the instruction pointer by 1,
-// but it halts the program instead.)
-//
-// "With terminology out of the way, we're ready to proceed.
-// To complete the gravity assist, you need to determine what pair of inputs
+
+// "...To complete the gravity assist, you need to determine what pair of inputs
 // produces the output 19690720."
 //
 // The inputs should still be provided to the program by replacing the values
