b4c33eba338d92caa692ba104cb0ae5e171c4b68
Pfeff Ákos György <ike86@users.noreply.github.com>
2020-12-02 22:50:22 +0100

    parse many rows

--- src/AoC20/AoC20/PasswordValidator.cs
+++ src/AoC20/AoC20/PasswordValidator.cs
@@ -25,6 +25,26 @@ namespace AoC20
                     new Policy(1, 3, 'a'),
                     "abcde"));
         }
+        
+        [Fact]
+        public void Test_parse_many()
+        {
+            var records =
+                Parse(Example)
+                    .ToArray();
+
+            records.Should().HaveCount(3);
+            records.Should().BeEquivalentTo(
+                new DatabaseRecord(
+                    new Policy(1, 3, 'a'),
+                    "abcde"),
+                new DatabaseRecord(
+                    new Policy(1, 3, 'b'),
+                    "cdefg"),
+                new DatabaseRecord(
+                    new Policy(2, 9, 'c'),
+                    "ccccccccc"));
+        }
 
         [Fact]
         public void Test()
@@ -36,13 +56,19 @@ namespace AoC20
 
         private IEnumerable<DatabaseRecord> Parse(string raw)
         {
-            var tokens = raw.Split(new[] {'-', ' ', ':'}, StringSplitOptions.RemoveEmptyEntries);
+            return raw.Split(Environment.NewLine)
+                .Select(line => ParseLine(line));
+        }
+
+        private DatabaseRecord ParseLine(string line)
+        {
+            var tokens = line.Split(new[] {'-', ' ', ':'}, StringSplitOptions.RemoveEmptyEntries);
             var min = int.Parse(tokens[0]);
             var max = int.Parse(tokens[1]);
             var letter = tokens[2].Single();
             var password = tokens[3];
 
-            yield return
+            return
                 new DatabaseRecord(
                     new Policy(min, max, letter),
                     password);
