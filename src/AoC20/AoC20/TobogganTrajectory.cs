80a67d2b130ef66a2cfda433a87d0438069550ee
Pfeff Ákos György <ike86@users.noreply.github.com>
2020-12-03 20:08:33 +0100

    Map_can_be_indexed

--- src/AoC20/AoC20/TobogganTrajectory.cs
+++ src/AoC20/AoC20/TobogganTrajectory.cs
@@ -27,10 +27,23 @@ namespace AoC20
             map.CountTreesOnSlope(right: 0, down: 1)
                 .Should().Be(3);
         }
+        
+        [Fact]
+        public void Map_can_be_indexed()
+        {
+            var map = new Map(Example);
+
+            map[0, 0].Should().Be(Map.Open);
+            map[0, 2].Should().Be(Map.Tree);
+            map[2, 0].Should().Be(Map.Open);
+        }
     }
 
     public class Map
     {
+        public static Square Open = new Open();
+        public static Square Tree = new Tree();
+        
         private readonly string[] _lines;
 
         public Map(string raw)
@@ -50,10 +63,64 @@ namespace AoC20
             }
             return count;
         }
+
+        public Square this[int i, int j]
+        {
+            get
+            {
+                if (_lines[i][j] == '.')
+                {
+                    return new Open();
+                }
+                else if (_lines[i][j] == '#')
+                {
+                    return new Tree();
+                }
+
+                throw new InvalidOperationException();
+            }
+        }
     }
 
-    public class TobogganTrajectory
+    public interface Square
     {
-        
+    }
+
+    public class Tree : Square, IEquatable<Tree>
+    {
+        public bool Equals(Tree other) => true;
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Tree) obj);
+        }
+
+        public override int GetHashCode() => typeof(Tree).GetHashCode();
+
+        public static bool operator ==(Tree left, Tree right) => Equals(left, right);
+
+        public static bool operator !=(Tree left, Tree right) => !Equals(left, right);
+    }
+
+    public class Open : Square, IEquatable<Open>
+    {
+        public bool Equals(Open other) => true;
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Open) obj);
+        }
+
+        public override int GetHashCode() => typeof(Open).GetHashCode();
+
+        public static bool operator ==(Open left, Open right) => Equals(left, right);
+
+        public static bool operator !=(Open left, Open right) => !Equals(left, right);
     }
 }
