1f1c8e72085246af30700939f8b96b12faaa78c6
Pfeff Ákos György <ike86@users.noreply.github.com>
2020-12-03 19:57:45 +0100

    day 3 part 1 - Map_CountTreesOnSlope_can_count_downwards

---
+++ src/AoC20/AoC20/TobogganTrajectory.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace AoC20
+{
+    public class Day03
+    {
+        private const string Example =
+@"..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#";
+        
+        [Fact]
+        public void Map_CountTreesOnSlope_can_count_downwards()
+        {
+            var map = new Map(Example);
+
+            map.CountTreesOnSlope(right: 0, down: 1)
+                .Should().Be(3);
+        }
+    }
+
+    public class Map
+    {
+        private readonly string[] _lines;
+
+        public Map(string raw)
+        {
+            _lines = raw.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public int CountTreesOnSlope(int right, int down)
+        {
+            var count = 0;
+            for (int i = 0; i < _lines.Length; i++)
+            {
+                if (_lines[i][0] == '#')
+                {
+                    count += 1;
+                }
+            }
+            return count;
+        }
+    }
+
+    public class TobogganTrajectory
+    {
+        
+    }
+}
