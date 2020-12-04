ad8e7b32f12b6fb6a9c2ccb52645a3dd0e0cf1b6
Pfeff Ákos György <ike86@users.noreply.github.com>
2020-12-04 23:55:49 +0100

    day 4 part 1 - can parse first line of first passport

---
+++ src/AoC20/AoC20/PassportProcessing.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace AoC20
+{
+    public class Day04
+    {
+        private const string Example =
+@"ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in";
+
+        [Fact]
+        public void Test()
+        {
+            IEnumerable<Passport> passports = Parse(Example);
+
+            passports.First().Should().BeEquivalentTo(
+                new Dictionary<string, string>
+                {
+                    ["ecl"] = "gry",
+                    ["pid"] = "860033327",
+                    ["eyr"] = "2020",
+                    ["hcl"] = "#fffffd",
+                });
+        }
+
+        private IEnumerable<Passport> Parse(string batch)
+        {
+            var l = batch.Split(Environment.NewLine).First();
+            yield return
+                new Passport(
+                    l.Split(" ")
+                        .Select(kvp =>
+                        {
+                            var tokens = kvp.Split(":");
+                            return (key: tokens[0], value: tokens[1]);
+                        }));
+        }
+    }
+
+    public class Passport : Dictionary<string, string>
+    {
+        public Passport(IEnumerable<(string key, string value)> kvps)
+        {
+            foreach (var kvp in kvps)
+            {
+                this.Add(kvp.key, kvp.value);
+            }
+        }
+    }
+
+    public class PassportProcessing
+    {
+    }
+}
