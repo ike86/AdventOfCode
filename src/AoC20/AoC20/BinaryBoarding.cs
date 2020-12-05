01b0afe61078a5aebdca123d5b7933f122dc410f
Pfeff Ákos György <ike86@users.noreply.github.com>
2020-12-05 17:55:11 +0100

    day 5 part 1 -  binary boarding can step one towards front

---
+++ src/AoC20/AoC20/BinaryBoarding.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace AoC20
+{
+    public class Day05
+    {
+        [Fact]
+        public void Step_one_towards_the_front()
+        {
+            var p = new BoardingPass("F");
+
+            p.Row.Should().Be(new Range(0, 63));
+        }
+    }
+
+    public class BoardingPass
+    {
+        public BoardingPass(string raw)
+        {
+            var start = 0;
+            var end = 127;
+            Row = new Range(start, end / 2);
+        }
+
+        public Range Row { get; set; }
+    }
+}
