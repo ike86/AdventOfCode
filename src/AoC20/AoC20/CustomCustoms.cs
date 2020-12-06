9b111bd18612a7c37109f409c2b803fe48157c71
Pfeff Ákos György <ike86@users.noreply.github.com>
2020-12-06 22:45:31 +0100

    solve day 6 part 1

--- src/AoC20/AoC20/CustomCustoms.cs
+++ src/AoC20/AoC20/CustomCustoms.cs
@@ -24,18 +24,65 @@ abcz";
             Sum(GroupOfMany).Should().Be(6);
         }
 
+        private const string ManyGroups =
+            @"abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b";
+        
+        [Fact]
+        public void Many_groups()
+        {
+            Sum(ManyGroups).Should().Be(11);
+        }
+        
+        [Fact]
+        public void Solve_puzzle()
+        {
+            Sum(PuzzleInput.ForDay06).Should().Be(6590);
+        }
+
         private int Sum(string raw)
         {
-            var yesAnswersOfGroupMembers = raw.Split(Environment.NewLine);
-            return
-                yesAnswersOfGroupMembers.Aggregate(
-                        seed: new HashSet<char>(),
-                        (set, yesAnswers) =>
+            var groups =
+                raw.Split(Environment.NewLine)
+                    .Aggregate(
+                        seed: new List<List<string>> {new List<string>()},
+                        (ll, line) =>
                         {
-                            set.UnionWith(new HashSet<char>(yesAnswers));
-                            return set;
-                        })
-                    .Count;
+                            if (line == string.Empty)
+                            {
+                                ll.Add(new List<string>());
+                            }
+                            else
+                            {
+                                ll.Last().Add(line);
+                            }
+
+                            return ll;
+                        });
+
+            return
+                groups.Sum(
+                    members => members.Aggregate(
+                            seed: new HashSet<char>(),
+                            (set, yesAnswers) =>
+                            {
+                                set.UnionWith(new HashSet<char>(yesAnswers));
+                                return set;
+                            })
+                        .Count);
         }
     }
 }
