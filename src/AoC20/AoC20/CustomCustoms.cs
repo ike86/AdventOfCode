c421ddf5b4ff6df63b4706fc2e741d1fdcbe0558
Pfeff Ákos György <ike86@users.noreply.github.com>
2020-12-06 22:19:20 +0100

    day 6 part 1
    - Group_of_one
    - Group_of_many

---
+++ src/AoC20/AoC20/CustomCustoms.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace AoC20
+{
+    public class Day06
+    {
+        [Fact]
+        public void Group_of_one()
+        {
+            Sum("abcx").Should().Be(4);
+        }
+        
+        private const string GroupOfMany = @"abcx
+abcy
+abcz";
+        
+        [Fact]
+        public void Group_of_many()
+        {
+            Sum(GroupOfMany).Should().Be(6);
+        }
+
+        private int Sum(string raw)
+        {
+            var yesAnswersOfGroupMembers = raw.Split(Environment.NewLine);
+            return
+                yesAnswersOfGroupMembers.Aggregate(
+                        seed: new HashSet<char>(),
+                        (set, yesAnswers) =>
+                        {
+                            set.UnionWith(new HashSet<char>(yesAnswers));
+                            return set;
+                        })
+                    .Count;
+        }
+    }
+}
