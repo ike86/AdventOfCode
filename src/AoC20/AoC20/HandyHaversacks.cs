9d01239d1a97ea810a5049c5b94f9ab3bcdc5a91
Pfeff Ákos György <ike86@users.noreply.github.com>
2020-12-10 19:26:56 +0100

    Can_parse_many_rules

--- src/AoC20/AoC20/HandyHaversacks.cs
+++ src/AoC20/AoC20/HandyHaversacks.cs
@@ -20,13 +20,20 @@ faded blue bags contain no other bags.
 dotted black bags contain no other bags.";
 
         [Fact]
-        public void Can_parse_a_rule()
+        public void Can_parse_many_rules()
         {
-            new RuleSet(Example).Rules.Should().BeEquivalentTo(
+            new RuleSet(Example).Rules.Take(3).Should().BeEquivalentTo(
                 new Rule(
                     "light red",
                     "1 bright white",
-                    "2 muted yellow"));
+                    "2 muted yellow"),
+                new Rule(
+                    "dark orange",
+                    "3 bright white",
+                    "4 muted yellow"),
+                new Rule(
+                    "bright white",
+                    "1 shiny gold"));
             // (number: 1, color: "bright white"),
             // (number: 2, color: "muted yellow")));
         }
@@ -47,20 +54,27 @@ dotted black bags contain no other bags.";
 
     public class RuleSet
     {
+        private static readonly IEnumerable<string> Separators = new[] {"bags contain", "bag,", "bags,", "bag.", "bags."};
+
         public RuleSet(string raw)
         {
+            Rules = ParseRules(raw);
+        }
+
+        private static Rule[] ParseRules(string raw)
+        {
+            return raw.Split(Environment.NewLine)
+                .Select(ParseRule)
+                .ToArray();
+        }
+
+        private static Rule ParseRule(string line)
+        {
             var tokens =
-                raw.Split(Environment.NewLine)
-                    .First()
-                    .Split(new[] {"bags contain", "bag,", "bags,", "bag.", "bags."},
-                        StringSplitOptions.RemoveEmptyEntries)
-                    .Select(s => s.Trim())
-                    .ToArray();
-            Rules =
-                new[]
-                {
-                    new Rule(tokens[0], tokens.Skip(1).Select(t => t).ToArray()),
-                };
+                line.Split(Separators.ToArray(), StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => s.Trim())
+                .ToArray();
+            return new Rule(tokens[0], tokens.Skip(1).Select(t => t).ToArray());
         }
 
         public IEnumerable<Rule> Rules { get; }
