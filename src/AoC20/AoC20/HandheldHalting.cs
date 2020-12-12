f5db9336c09646df0ce96deb952ae1b321341aa6
Pfeff Ákos György <ike86@users.noreply.github.com>
2020-12-12 17:48:12 +0100

    refactor - use union type

--- src/AoC20/AoC20/HandheldHalting.cs
+++ src/AoC20/AoC20/HandheldHalting.cs
@@ -22,52 +22,79 @@ acc +6";
         [Fact]
         public void Can_parse_Noop()
         {
-            new BootCode(Exmaple).Instructions.Take(1)
-                .Should().BeEquivalentTo(
-                    new Instruction("nop", 0));
+            new BootCode(Exmaple).Instructions.First()
+                .Should().BeOfType(typeof(Noop));
         }
-        
+
         [Fact]
         public void Can_parse_Accumulate()
         {
             new BootCode(Exmaple).Instructions.Skip(1).Take(1)
-                .Should().BeEquivalentTo(
-                    new Instruction("acc", 1));
+                .Should().BeEquivalentTo(new Accumulate(1));
         }
-        
+
         [Fact]
         public void Can_parse_Jump()
         {
             new BootCode(Exmaple).Instructions.Skip(2).Take(1)
-                .Should().BeEquivalentTo(
-                    new Instruction("jmp", 4));
+                .Should().BeEquivalentTo(new Jump(4));
         }
     }
 
-    public class Instruction
+    public interface IInstruction
     {
-        public Instruction(string instructionCode, int i)
+    }
+
+    public class Noop : IInstruction
+    {
+    }
+
+    public class Accumulate : IInstruction
+    {
+        public Accumulate(int change)
         {
-            InstructionCode = instructionCode;
-            I = i;
+            Change = change;
         }
 
-        public string InstructionCode { get; }
+        public int Change { get; }
+    }
 
-        public int I { get; }
+    public class Jump : IInstruction
+    {
+        public Jump(int offset)
+        {
+            Offset = offset;
+        }
+
+        public int Offset { get; }
     }
 
     public class BootCode
     {
         public BootCode(string raw)
         {
-            Instructions =
-                raw.Split(Environment.NewLine)
-                    .Select(line => line.Split(' '))
-                    .Select(tokens => new Instruction(tokens[0], int.Parse(tokens[1])))
-                    .ToArray();
+            Instructions = ParseInstructions(raw);
         }
 
-        public IEnumerable<Instruction> Instructions { get; }
+        public IEnumerable<IInstruction> Instructions { get; }
+        
+        private static IInstruction[] ParseInstructions(string raw)
+        {
+            return raw.Split(Environment.NewLine)
+                .Select(line => line.Split(' '))
+                .Select(ParseInstruction)
+                .ToArray();
+        }
+
+        private static IInstruction ParseInstruction(string[] tokens)
+        {
+            return tokens[0] switch
+            {
+                "nop" => new Noop(),
+                "jmp" => new Jump(int.Parse(tokens[1])),
+                "acc" => new Accumulate(int.Parse(tokens[1])),
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
     }
 }
