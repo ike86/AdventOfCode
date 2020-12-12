f5c9a8b61b00c3bc67aa127fe5f3e93a99c8cba5
Pfeff Ákos György <ike86@users.noreply.github.com>
2020-12-12 17:23:36 +0100

    day 8 part 1 - Can_parse_Noop

---
+++ src/AoC20/AoC20/HandheldHalting.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace AoC20
+{
+    public class Day08
+    {
+        private const string Exmaple =
+            @"nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6";
+
+        [Fact]
+        public void Can_parse_Noop()
+        {
+            new BootCode(Exmaple).Instructions
+                .Should().BeEquivalentTo(
+                    new Instruction("nop", 0));
+        }
+    }
+
+    public class Instruction
+    {
+        public Instruction(string instructionCode, int i)
+        {
+            InstructionCode = instructionCode;
+            I = i;
+        }
+
+        public string InstructionCode { get; }
+
+        public int I { get; }
+    }
+
+    public class BootCode
+    {
+        public BootCode(string raw)
+        {
+            var tokens =
+                raw.Split(Environment.NewLine)
+                .First()
+                .Split(' ');
+            Instructions = new[] { new Instruction(tokens[0], int.Parse(tokens[1])) };
+        }
+
+        public IEnumerable<Instruction> Instructions { get; }
+    }
+}
