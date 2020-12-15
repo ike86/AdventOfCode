2bb1f65e115703e1d78e1e5f44e547c58e1f5d79
Pfeff Ákos György <ike86@users.noreply.github.com>
2020-12-15 19:33:01 +0100

    WIP GetFirstNotSumOfPrevious_three_returns_first_outlier

--- src/AoC20/AoC20/EncodingError.cs
+++ src/AoC20/AoC20/EncodingError.cs
@@ -1,4 +1,7 @@
-﻿using FluentAssertions;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using AutoFixture.Xunit2;
+using FluentAssertions;
 using Xunit;
 
 namespace AoC20
@@ -31,6 +34,16 @@ namespace AoC20
         public void GetFirstNotSumOfPrevious_five_returns_expected_for_the_example()
         {
             new Decoder(Example).GetFirstNotSumOfPrevious(5).Should().Be(127);
+        }
+
+        [Theory, AutoData]
+        public void GetFirstNotSumOfPrevious_three_returns_first_outlier(int[] preamble, [Range(0, 9 - 3 - 1)] int i)
+        {
+            var possibleSums =
+                preamble.Join(preamble, _ => 0, _ => 0, (a, b) => (a, b))
+                    .Where(tuple => tuple.a != tuple.b)
+                    .Select(tuple => tuple.a + tuple.b);
+            possibleSums.Should().HaveCount(6);
         }
     }
 
