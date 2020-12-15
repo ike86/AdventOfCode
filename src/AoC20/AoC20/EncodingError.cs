542cc9e669a7149e05cb7ca520dae0355a025e9d
Pfeff Ákos György <ike86@users.noreply.github.com>
2020-12-15 19:25:15 +0100

    day 9 part 1 - add failing, skipped GetFirstNotSumOfPrevious_five_returns_expected_for_the_example

---
+++ src/AoC20/AoC20/EncodingError.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace AoC20
+{
+    public class Day09
+    {
+        private const string Example =
+            @"35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576";
+        
+        [Fact(Skip = "This is a high level goal")]
+        public void GetFirstNotSumOfPrevious_five_returns_expected_for_the_example()
+        {
+            new Decoder(Example).GetFirstNotSumOfPrevious(5).Should().Be(127);
+        }
+    }
+
+    public class Decoder
+    {
+        public Decoder(string raw)
+        {
+            
+        }
+
+        public int GetFirstNotSumOfPrevious(int n)
+        {
+            return default;
+        }
+    }
+}
