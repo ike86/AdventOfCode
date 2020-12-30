0e06f14e1cb4fe7f527a4902198eb203f5cd1db0
Pfeff Ákos György <ike86@users.noreply.github.com>
2020-12-30 15:08:28 +0100

    day 11 part 1 - parse empty seat

--- src/AoC20/AoC20/SeatingSystem.cs
+++ src/AoC20/AoC20/SeatingSystem.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
 using Xunit;
 
 namespace AoC20
@@ -6,10 +7,10 @@ namespace AoC20
     public class Day11
     {
         [Fact]
-        public void Floor()
-        {
-            new WaitingArea(".")[0, 0].Should().Be(WaitingArea.Floor);
-        }
+        public void Floor() => new WaitingArea(".")[0, 0].Should().Be(WaitingArea.Floor);
+        
+        [Fact]
+        public void Empty_seat() => new WaitingArea("L")[0, 0].Should().Be(WaitingArea.EmptySeat);
     }
 
     public class WaitingArea
@@ -18,10 +19,22 @@ namespace AoC20
 
         public WaitingArea(string rawInitialLayout)
         {
-            _positions = new[] {new[] {Floor}};
+            _positions = new[] {new[] {ParsePosition(rawInitialLayout)}};
+        }
+
+        private static IPosition ParsePosition(string rawPosition)
+        {
+            if (rawPosition == ".")
+                return Floor;
+            else if(rawPosition == "L")
+                return EmptySeat;
+            else
+                throw new ArgumentOutOfRangeException();
         }
 
         public static IPosition Floor { get; } = new Floor();
+        
+        public static IPosition EmptySeat { get; } = new EmptySeat();
 
         public IPosition this[int i, int j] => _positions[i][j];
     }
@@ -31,6 +44,10 @@ namespace AoC20
     }
 
     public class Floor : IPosition
+    {
+    }
+
+    public class EmptySeat : IPosition
     {
     }
 }
