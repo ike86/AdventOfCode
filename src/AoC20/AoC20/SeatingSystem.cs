fcdf333a0887fe715a7c3e53b062814281f947e9
Pfeff Ákos György <ike86@users.noreply.github.com>
2020-12-30 14:59:30 +0100

    day 11 part 1 - parse floor

---
+++ src/AoC20/AoC20/SeatingSystem.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace AoC20
+{
+    public class Day11
+    {
+        [Fact]
+        public void Floor()
+        {
+            new WaitingArea(".")[0, 0].Should().Be(WaitingArea.Floor);
+        }
+    }
+
+    public class WaitingArea
+    {
+        private readonly IPosition[][] _positions;
+
+        public WaitingArea(string rawInitialLayout)
+        {
+            _positions = new[] {new[] {Floor}};
+        }
+
+        public static IPosition Floor { get; } = new Floor();
+
+        public IPosition this[int i, int j] => _positions[i][j];
+    }
+
+    public interface IPosition
+    {
+    }
+
+    public class Floor : IPosition
+    {
+    }
+}
