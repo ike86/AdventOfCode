17b55544467ef1d0c9cfe1c24630d7b4111aee4f
Pfeff Ákos György <ike86@users.noreply.github.com>
2022-12-11 17:01:33 +0100

    day 01 part 2 failing test for example

---
+++ src/AoC22.Fs/AoC22.Fs/Day01.Part2.fs
@@ -0,0 +1,20 @@
+﻿namespace AoC22.Fs.Day01
+
+module Part2 =
+    let topThreeMostCalories (maybeCalories: int option []) : int =
+        42
+
+    // In the example above, the top three Elves are the fourth Elf
+    // (with 24000 Calories), then the third Elf (with 11000 Calories),
+    // then the fifth Elf (with 10000 Calories).
+    // The sum of the Calories carried by these three elves is 45000.
+
+    open Xunit
+    open FsUnit.Xunit
+
+    module ``Given a calories inventory list`` =
+        [<Fact>]
+        let ``topThreeMostCalories carried by an elf for Example`` () =
+            parse Example.input
+            |> topThreeMostCalories
+            |> should equal 45000
