2897309fd2fcf9328564d4f121a1d91ac0f5b612
Pfeff Ákos György <ike86@users.noreply.github.com>
2022-12-11 17:54:25 +0100

    solve day 02 failing test for parse

---
+++ src/AoC22.Fs/AoC22.Fs/Day02.fs
@@ -0,0 +1,26 @@
+﻿namespace AoC22.Fs
+
+module Day02 =
+
+    type HandShape = Rock | Paper | Scissor
+
+    let parse _ = Array.empty
+
+    module Example =
+        let input =
+            "A Y\n\
+             B X\n\
+             C Z"
+
+    open Xunit
+    open FsUnit.Xunit
+
+    module ``Given a strategy guide`` =
+        [<Fact>]
+        let ``parse yields the list of hand shape pairs`` () =
+            parse Example.input
+            |> should
+                equal
+                [| (Rock, Paper)
+                   (Paper, Rock)
+                   (Scissor, Scissor) |]
